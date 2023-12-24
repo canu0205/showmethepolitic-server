0913a477e0fac6ab0772aec9673f890d5e2ee2e7
canu <cksdncjf@naver.com>
2023-12-24 15:59:30 +0900

    Add: bucket api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const users = require("./users.route");
+const bucket = require("./bucket.route");
+
+router.use("/users", users);
+router.use("/bucket", bucket);
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get("/", function (req, res, next) {
+//   res.render("index", { title: "Express" });
+// });
 
 module.exports = router;
