b017c06efd70f2a75e24198248f1e11870ab0c43
moyedx3 <samoyedali@gmail.com>
2023-12-25 15:57:59 +0900

    get file link

--- controllers/bucket.controller.js
+++ controllers/bucket.controller.js
@@ -79,6 +79,55 @@ module.exports = {
     }
   },
 
+  listFileNames: async (req, res) => {
+    try {
+      const S3 = new AWS.S3({
+        endpoint,
+        region,
+        credentials: {
+          accessKeyId: access_key,
+          secretAccessKey: secret_key,
+        },
+      });
+
+      const bucket_name = req.body.bucket_name;
+      if (!bucket_name) {
+        return res.status(400).json({ message: "Bucket name is required" });
+      }
+
+      const MAX_KEYS = 300;
+
+      let params = {
+        Bucket: bucket_name,
+        MaxKeys: MAX_KEYS
+      };
+
+      console.log("Listing All Files in Bucket: " + bucket_name);
+      console.log("==========================");
+
+      while (true) {
+        let response = await S3.listObjectsV2(params).promise();
+
+        for (let content of response.Contents) {
+          const fileUrl = `https://kr.object.ncloudstorage.com/${bucket_name}/${content.Key}`;
+          console.log(`File URL: ${fileUrl}`);
+        }
+
+        if (response.IsTruncated) {
+          params.ContinuationToken = response.NextContinuationToken;
+        } else {
+          break;
+        }
+      }
+
+      return res.status(200).json({ message: "Files listed successfully" });
+
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ message: err.message });
+    }
+  },
+
   setCors: async (req, res, next) => {
     try {
       const S3 = new AWS.S3({
--- routes/bucket.route.js
+++ routes/bucket.route.js
@@ -8,10 +8,8 @@ router.post("/create", controller.createBucket, async (req, res, next) => {});
 router.put("/set-cors", controller.setCors, async (req, res, next) => {});
 router.put("/update-acl", controller.updateAcl, async (req, res, next) => {});
 
-router.post(
-  "/upload-from-youtube",
-  controller.uploadFile,
-  async (req, res, next) => {}
-);
+router.post("/upload-from-youtube", controller.uploadFile, async (req, res, next) => {});
+
+router.post("/list-file-names",controller.listFileNames,async (req, res, next) => {});
 
 module.exports = router;
